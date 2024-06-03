010a2330dc15e800b45a58a8a1ca80c883946cd3
Eliel Alves <elielalves.dev@gmail.com>
2024-06-03 15:44:14 -0300

    feat: add context of transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react";
-
 import { Container } from "./styles";
-import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 
-interface TransactionProps {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-}
 
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<TransactionProps[]>([]);
-
-  useEffect(() => {
-    api.get('/transactions')
-      .then(response => setTransactions(response.data.transactions))
-  }, []);
+  const { transactions } = useTransactions();
 
   return (
     <Container>
